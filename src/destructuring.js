befb271046a392f06f77b09acb7f562bf7ca62b7
Sergey Yushmanov <s.yushmanov777@gmail.com>
2018-04-23 23:29:45 +0700

    work with destructuring

---
+++ src/destructuring.js
@@ -0,0 +1,33 @@
+/*let [js, php, py, rb] = ["JavaScript", "PHP", "Python", "Ruby"];
+
+console.log(js, php, py, rb);
+
+let scores = [3, 4, [5, 6]];
+
+let [low, mid, [high, higher]] = scores;
+
+console.log(low, mid, high, higher);*/
+
+function computeScore([low, mid]) {
+  console.log(low, mid);
+}
+
+//computeScore([3, 4]);
+
+function getScores() {
+  return [3, 4, 5];
+}
+
+let scores = getScores();
+
+console.log(scores);
+
+let [low, mid, high] = getScores();
+
+console.log(low, mid, high);
+
+let yes = "Yes", no = "No";
+
+[yes, no] = [no, yes];
+
+console.log(yes, no);
