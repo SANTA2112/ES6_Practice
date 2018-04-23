0eca6cb23377f2af9338fba0fe9cc99ac743d557
Sergey Yushmanov <s.yushmanov777@gmail.com>
2018-04-23 16:24:05 +0700

    we have problems with the global variable

---
+++ dist/let.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var buttons = document.querySelectorAll("button");
+
+for (var i = 0; i < buttons.length; i++) {
+  var button = buttons[i];
+  button.innerText = i;
+  button.onclick = function (e) {
+    console.log(i);
+  };
+}
