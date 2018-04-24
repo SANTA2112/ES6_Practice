82b14c3099b5f704d1d8fed8aa114b9253baa0a5
Sergey Yushmanov <s.yushmanov777@gmail.com>
2018-04-25 02:13:20 +0700

    work with generators part2

---
+++ src/generators creating.js
@@ -0,0 +1,18 @@
+
+function* generator() {
+  try {
+    yield 1;
+    yield 2;
+    yield 3;
+  } catch(e) {
+    console.log(e);
+  }
+
+
+}
+
+let iterator = generator();
+console.log(iterator.next());
+console.log(iterator.return());
+console.log(iterator.throw(new Error("Error")));
+console.log(iterator.next());
